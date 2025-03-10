8ce2fc1ae6b5108d8b3b12ed3cc6ff4e60b57041
Munkhtemuulen Khashbolor <munhtemuulen.kh2002@gmail.com>
2025-03-10 15:01:51 +0900

    tasks completed, failed at findPrime problem

--- script.js
+++ script.js
@@ -1,17 +1,53 @@
 "use strict";
 
-const output = document.querySelector(".output");
+// even numbers
 
-for (let i = 10; i >= 0; i--) {
-  setTimeout(() => {
-    if (i == 0) {
-      const blastOff = document.createElement("p");
-      blastOff.textContent = "Blast Off !!!";
-      output.appendChild(blastOff);
-    } else {
-      const countdown = document.createElement("p");
-      countdown.textContent = `Countdown ${i}`;
-      output.appendChild(countdown);
+for (let i = 1; i < 11; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}
+
+// replace "for" with "while"
+
+for (let i = 0; i < 3; i++) {
+  console.log(`number ${i}!`, "for loop");
+}
+
+let i = 0;
+while (i < 3) {
+  console.log(`number ${i}!`, "while loop");
+  i++;
+}
+
+// repeat until the input is correct
+
+let condition = false;
+
+while (condition) {
+  const answer = Number(prompt("Write number higher than 100!"));
+  if (answer >= 100) {
+    alert("good job!");
+    condition = false;
+  } else {
+    alert("higher than 100!!!");
+  }
+}
+
+console.log("-------");
+// Outputs prime numbers
+
+const target = Number(prompt("Enter the range!"));
+
+for (let i = 1; i < target; i++) {
+  if (i == 1) {
+    console.log(1);
+    continue;
+  }
+  for (let a = 2; a < i; a++) {
+    if (i % a == 0) {
+      continue;
     }
-  }, (10 - i) * 1000);
+  }
+  console.log(i);
 }
